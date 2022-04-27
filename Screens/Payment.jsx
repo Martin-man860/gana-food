ea8ae6ae1b09bc70d160710e2a4c52cc7f4a5071
Martin Robertson <martinrobertson860@gmail.com>
2022-04-27 03:36:03 +0000

    done

--- Screens/Payment.jsx
+++ Screens/Payment.jsx
@@ -5,6 +5,7 @@ const Payment = () => {
   return (
     <View style={styles.Container}>
       
+    <Text style={styles.text}></Text> 
     </View>
   )
 }
@@ -15,6 +16,16 @@ const styles = StyleSheet.create({
   Container:{
     backgroundColor:"#2F393D",
     flex:1,
-    
+    justifyContent:"center"
+  },
+
+  text:{
+    fontSize:50,
+    color:"black",
+    flex:1,
+    borderRadius:35,
+    marginTop:540,
+    textAlign:"center",
+    backgroundColor:"white"
   }
 })
